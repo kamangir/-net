6ce1e56e0d14927915c1850b35df18240385e152
kamangir <arash@kamangir.net>
2025-11-18 11:19:02 +0330

    ++ - kamangir/bolt#746

--- bluer_plugin/docs/README.md
+++
@@ -1,5 +0,0 @@
-# 
-
-- [aliases](./aliases)
-- [feature-1](./feature_1)
-- [feature-2](./feature_2.md)
---
+++ kamangir_net/__init__.py
@@ -0,0 +1,18 @@
+NAME = "kamangir_net"
+
+ICON = "📜"
+
+DESCRIPTION = f"{ICON} kamangir writes here."
+
+VERSION = "5.2.1"
+
+REPO_NAME = "-net"
+
+MARQUEE = "https://github.com/kamangir/assets2/blob/main/blog/blog.png?raw=true"
+
+
+ALIAS = "@blog"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
